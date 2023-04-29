cdacb1781c3290c22dca41c9b5c9778c48091601
Kamil_Kuc <k.kuc.gm@gmail.com>
2023-04-29 21:15:12 +0200

    dodanie layoutu i podłączenie do mysql

--- ZastepcaProcesowy/app.py
+++ ZastepcaProcesowy/app.py
@@ -1,19 +1,62 @@
-"""
-This script runs the application using a development server.
-It contains the definition of routes and views for the application.
-"""
+from flask import Flask, flash, redirect, render_template, session
+from flask_session import Session
+from tempfile import mkdtemp
+from werkzeug.security import check_password_hash, generate_password_hash
+from datetime import datetime
+from helpers import apology, login_required
+import mysql.connector
+from mysql.connector import Error
 
-from flask import Flask
+
 app = Flask(__name__)
 
+# Ensure templates are auto-reloaded
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+
+
+# Configure session to use filesystem (instead of signed cookies)
+
+#app.config["SESSION_PERMANENT"] = False
+#app.config["SESSION_TYPE"] = "filesystem"
+#Session(app)
+
+
 # Make the WSGI interface available at the top level so wfastcgi can get it.
-wsgi_app = app.wsgi_app
+#wsgi_app = app.wsgi_app
+
+
+# Ensure templates are auto-reloaded
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+
+# Connect to MySQL
+try:
+    db = mysql.connector.connect(host='localhost',
+                                         database='zastepca',
+                                         user='admin',
+                                         password='niepamietam100%')
+    if db.is_connected():
+        db_Info = db.get_server_info()
+        print("Connected to MySQL Server version ", db_Info)
+        cursor = db.cursor()
+        cursor.execute("select database();")
+        record = cursor.fetchone()
+        print("You're connected to database: ", record)
+
+except Error as e:
+    print("Error while connecting to MySQL", e)
+finally:
+    if db.is_connected():
+        cursor.close()
+        db.close()
+        print("MySQL connection is closed")
+
 
 
 @app.route('/')
-def hello():
-    """Renders a sample page."""
-    return "Hello World!"
+def index():
+    return render_template("layout.html")
+
+
 
 if __name__ == '__main__':
     import os
@@ -22,4 +65,15 @@ if __name__ == '__main__':
         PORT = int(os.environ.get('SERVER_PORT', '5555'))
     except ValueError:
         PORT = 5555
+    
     app.run(HOST, PORT)
+
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    """Register user"""
+    if request.method == "GET":
+        return render_template("register.html")
+
+

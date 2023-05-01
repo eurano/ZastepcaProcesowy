b8e05089eacaa2fc77d42ec7b18a3a44018bb1ea
Kamil_Kuc <k.kuc.gm@gmail.com>
2023-05-01 17:32:26 +0200

    Zakładanie konta

--- ZastepcaProcesowy/app.py
+++ ZastepcaProcesowy/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, flash, redirect, render_template, session
+﻿from flask import Flask, flash, url_for, redirect, render_template, request, session
 from flask_session import Session
 from tempfile import mkdtemp
 from werkzeug.security import check_password_hash, generate_password_hash
@@ -6,12 +6,18 @@ from datetime import datetime
 from helpers import apology, login_required
 import mysql.connector
 from mysql.connector import Error
+from forms import RegistrationForm, LoginForm
+
 
 
 app = Flask(__name__)
 
+# DELETE THIS BEFORE DEPLOYMENT !!!!!
+app.config['DEBUG'] = True
+
 # Ensure templates are auto-reloaded
 app.config["TEMPLATES_AUTO_RELOAD"] = True
+app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
 
 
 # Configure session to use filesystem (instead of signed cookies)
@@ -34,23 +40,113 @@ try:
                                          database='zastepca',
                                          user='admin',
                                          password='niepamietam100%')
+
     if db.is_connected():
         db_Info = db.get_server_info()
         print("Connected to MySQL Server version ", db_Info)
-        cursor = db.cursor()
-        cursor.execute("select database();")
-        record = cursor.fetchone()
-        print("You're connected to database: ", record)
 
 except Error as e:
     print("Error while connecting to MySQL", e)
 
 
+cursor = db.cursor()
+cursor = db.cursor(prepared=True)
+cursor = db.cursor(buffered=True)
 
 
-@app.route('/')
-def index():
-    return render_template("layout.html")
+posts = [
+    {
+        'author': 'Corey Schafer',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'April 20, 2018'
+    },
+    {
+        'author': 'Jane Doe',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'April 21, 2018'
+    }
+]
+
+
+
+@app.route("/")
+@app.route("/home")
+def home():
+    return render_template('home.html', posts=posts)
+
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    print(form.username.data)
+
+    if form.validate_on_submit():
+
+        # Insert new user to DB
+        # Ensure username does not exist
+        sql = """ SELECT * FROM users WHERE username = %s """
+        values = (form.username.data,)
+        cursor.execute(sql, values)
+        db.commit()
+        rows = cursor.rowcount
+        if rows != 0:
+            flash('Ta nazwa użytkownika jest zajęta', 'danger')
+            return render_template('register.html', title='Register', form=form)
+        else:
+            # Add user to database
+            username = form.username.data
+            now = datetime.now()
+            hash = generate_password_hash(username, method='pbkdf2:sha256', salt_length=8)
+            sql = """ INSERT INTO users (username, created_at, hash, last_active) VALUES (%s,%s,%s,%s) """
+            values = (username, now, hash, now)
+            cursor.execute(sql, values)
+            db.commit()
+            # Check if DB operation was succesfull
+            if cursor.rowcount != 0:
+                flash('Registration succesful! You can log in now', 'info')
+                return render_template('login.html', title='Login', form=form)
+            else:
+                flash('Konto nie zostało utworzone z powodu błędu', 'error')
+                return render_template('register.html', title='Register', form=form)
+
+    return render_template('register.html', title='Register', form=form)
+
+
+
+@app.route("/about")
+def about():
+    return render_template('about.html', title='About')
+
+
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
+            flash('You have been logged in!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login Unsuccessful. Please check username and password', 'danger')
+    return render_template('login.html', title='Login', form=form)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -63,21 +159,3 @@ if __name__ == '__main__':
         PORT = 5555
     
     app.run(HOST, PORT)
-
-
-
-@app.route("/register", methods=["GET", "POST"])
-def register():
-    """Register user"""
-    if request.method == "GET":
-        return render_template("register.html")
-
-
-
-
-@app.route("/logout", methods=["GET", "POST"])
-def logout():
-
-        cursor.close()
-        db.close()
-        print("MySQL connection is closed")
---
+++ ZastepcaProcesowy/forms.py
@@ -0,0 +1,48 @@
+﻿from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, SelectField
+from wtforms.validators import DataRequired, Length, Email, EqualTo
+
+
+class RegistrationForm(FlaskForm):
+
+
+    username = StringField('Nazwa użytkownika',
+                           validators=[DataRequired(), Length(min=2, max=50)])
+
+    email = StringField("Email", validators=[DataRequired(), Email()])
+
+    name = StringField('Imię',
+                        validators=[DataRequired(), Length(min=2, max=25)])
+    lastname = StringField('Nazwisko',
+                        validators=[DataRequired(), Length(min=2, max=35)])
+
+    phonenumber = StringField('Numer telefonu',
+                        validators=[DataRequired(), Length(min=8, max=12)])
+
+    profession = SelectField(u'Zawód', choices=[(None, ''), ("Adwokat", "Adwokat"), ("Radca prawny", "Radca prawny"), ("Rzecznik patentowy", "Rzecznik patentowy"),
+                                               ("Aplikant adwokacki", "Aplikant adwokacki"), ("Aplikant radcowski", "Aplikant radcowski"), ("Aplikant rzecznikowski",
+                                                "Aplikant rzecznikowski"), ("inne", "inne")])
+
+    region = SelectField(u'Apelacja', choices=[(None, ''), ("Szczecin", "Szczecin"), ("Gdańsk", "Gdańsk"), ("Poznań", "Poznań"), ("Białystok", "Białystok"),
+                                              ("Warszawa", "Warszawa"), ("Łódź", "Łódź"), ("Katowice", "Katowice"), ("Wrocław", "Wrocław"), ("Kraków", "Kraków"),
+                                              ("Lublin", "Lublin"), ("Rzeszów", "Rzeszów")])                                   
+    town = StringField('Miasto',
+                              validators=[DataRequired(), Length(min=2, max=30)])
+
+    password = PasswordField('Hasło', validators=[DataRequired()])
+
+    confirm_password = PasswordField('Potwierdź hasło',
+                                     validators=[DataRequired(), EqualTo('password')])
+
+    submit = SubmitField('Zarejstruj się')
+
+
+class LoginForm(FlaskForm):
+
+
+    username = StringField('Nazwa użytkownika',
+                        validators=[DataRequired()])
+    password = PasswordField('Hasło', validators=[DataRequired()])
+    submit = SubmitField('Zaloguj się')
+
+
